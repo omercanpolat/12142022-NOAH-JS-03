14725507ccca917f3dadf5f45d9e2c1b2a68bb8e
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2022-12-15 12:45:16 +0300

    son

--- script.js
+++ script.js
@@ -254,18 +254,18 @@
 //   console.log("Your salary will not change.");
 // }
 
-const num1 = Number(prompt("Please enter the first number"));
-const operator = prompt("Enter operator (either +, -, * or /)");
-const num2 = Number(prompt("Please enter the second number"));
+// const num1 = Number(prompt("Please enter the first number"));
+// const operator = prompt("Enter operator (either +, -, * or /)");
+// const num2 = Number(prompt("Please enter the second number"));
 
-let result;
+// let result;
 
-if (operator == "+") result = num1 + num2;
-else if (operator == "-") result = num1 - num2;
-else if (operator == "*") result = num1 * num2;
-else if (operator == "/") result = num1 / num2;
-else {
-  console.log("Please choose one of the operator above.");
-}
+// if (operator == "+") result = num1 + num2;
+// else if (operator == "-") result = num1 - num2;
+// else if (operator == "*") result = num1 * num2;
+// else if (operator == "/") result = num1 / num2;
+// else {
+//   console.log("Please choose one of the operator above.");
+// }
 
-console.log(result);
+// console.log(result);
